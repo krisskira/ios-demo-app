11abc7b2e7fdb603c4725003637cab4633afdad0
Crhistian David Vergara Gomez <crhistian.vergara@sigma3ds.com>
2020-07-01 19:36:36 -0500

    Initial Commit

---
+++ ceiba soft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ceiba soft
+//
+//  Created by Crhistian Vergara Gomez on 1/07/20.
+//  Copyright © 2020 Crhistian Vergara Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ceiba softTests/ceiba_softTests.swift
@@ -0,0 +1,34 @@
+//
+//  ceiba_softTests.swift
+//  ceiba softTests
+//
+//  Created by Crhistian Vergara Gomez on 1/07/20.
+//  Copyright © 2020 Crhistian Vergara Gomez. All rights reserved.
+//
+
+import XCTest
+@testable import ceiba_soft
+
+class ceiba_softTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
